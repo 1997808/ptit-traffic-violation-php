58c0675b67617e275adaab80a59c71f43b631c76
1997808 <khanhhuyle22@gmail.com>
2021-12-02 13:04:04 +0700

    cors gone

--- api/user/updateUser.php
+++ api/user/updateUser.php
@@ -1,6 +1,9 @@
 <?php
-  header('Access-Control-Allow-Origin:*');
+  header('Access-Control-Allow-Origin: http://localhost:3000');
   header('Content-Type: application/json');
+  header('Access-Control-Allow-Headers: true', 'Origin, X-Requested-With, Content-Type, Accept');
+  header('Access-Control-Allow-Credentials: true');
+  
   include_once('../../config/db.php');
   include_once('../../model/user.php');
 
